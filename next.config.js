ac97af25f1f307314f51d558f244585aeb97a389
Janne Peippo <peippo@gmail.com>
2022-03-26 11:25:52 +0200

    Add svgr

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ module.exports = {
   compiler: {
     styledComponents: true,
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
